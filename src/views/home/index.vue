f691d0a544c2303b7ad9c1e3e52c651940e8d443
menglv <menglv@sensetime.com>
2021-08-17 09:53:49 +0800

    feat: 添加确认周报功能

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,23 @@
 <template>
     <div class='container'>
         <h1>周报</h1>
-        <rz-radio-group class='radio-view' v-model="username" size="large">
-            <rz-radio-button label="孟律"></rz-radio-button>
-            <rz-radio-button label="冉旸旸"></rz-radio-button>
-            <rz-radio-button label="张义辉"></rz-radio-button>
-            <rz-radio-button label="李峙昊"></rz-radio-button>
-        </rz-radio-group>
+        <div class="manipulate">
+            <rz-radio-group class='radio-view' v-model="username" size="large">
+                <rz-radio-button label="孟律"></rz-radio-button>
+                <rz-radio-button label="冉旸旸"></rz-radio-button>
+                <rz-radio-button label="张义辉"></rz-radio-button>
+                <rz-radio-button label="李峙昊"></rz-radio-button>
+            </rz-radio-group>
+            <rz-button size='large' @click="handleSubmit">提交</rz-button>
+        </div>
         <mavon-editor v-model="content" />
-        <rz-button class='button' size='large' @click="handleSubmit">提交</rz-button>
+        <rz-dialog title="提示" :visible.sync="dialogVisible" body-padding width="30%" :before-close="handleClose" v-loading="loading">
+            <vue-markdown class="row-item-content">{{ postMsg }}</vue-markdown>
+            <span slot="footer" class="dialog-footer">
+                <rz-button type="info" @click="cancelSubmit">取 消</rz-button>
+                <rz-button type="primary" @click="confirmSubmit">确 定</rz-button>
+              </span>
+        </rz-dialog>
     </div>
 </template>
 
@@ -16,18 +25,31 @@
 import Bmob from "hydrogen-js-sdk"
 import _ from 'lodash'
 import mavonEditor from 'mavon-editor'
+import VueMarkdown from 'vue-markdown'
 import 'mavon-editor/dist/css/index.css'
 import { sendReport } from 'services'
 
 export default {
     name: 'Home',
     components: {
-        'mavon-editor': mavonEditor.mavonEditor
+        'mavon-editor': mavonEditor.mavonEditor,
+        'vue-markdown': VueMarkdown
     },
     data() {
         return {
             username: '',
-            content: ''
+            content: `### 本周完成 
+1. ~
+2. ~
+### 下周计划 
+1. ~
+2. ~
+### 问题/风险 
+1. ~
+2. ~`,
+            postMsg: '',
+            dialogVisible: false,
+            loading: false
         }
     },
     methods: {
@@ -38,8 +60,10 @@ export default {
             query.set("content", this.content)
             query.save().then(res => {
                 console.log('=====>', res)
+                return null
             }).catch(err => {
                 console.log('------>', err)
+                return err
             })
         },
 
@@ -56,16 +80,37 @@ export default {
             // window.location.href = window.location.origin + '/record?name=' + this.username
             // return
             // eslint-disable-next-line no-unreachable
-            const res = `[${this.username}的周报](${window.location.origin}/record?name=${this.username}) \n ${this.content}`
+            this.postMsg = `[${this.username}的周报](${window.location.origin}/record?name=${this.username}) \n ${this.content}`
+            this.dialogVisible = true
+        },
+
+        cancelSubmit() {
+            this.dialogVisible = false
+            this.postMsg = ''
+        },
+
+        async confirmSubmit() {
+
             sendReport({
                 "msgtype": "markdown",
                 "markdown": {
-                    "content": res
+                    "content": this.postMsg
                 }
             })
 
-            this.saveData()
+            this.loading = true
+            const err = await this.saveData()
+            this.loading = false
+            if (err) {
+                this.$message.error(err)
+            } else {
+                this.$message('保存成功')
+                this.dialogVisible = false
+                this.postMsg = ''
+            }
         }
+
+
     }
 }
 </script>
@@ -76,11 +121,13 @@ export default {
     padding: 60px;
 }
 
-.radio-view {
-    margin: 30px 0;
+.manipulate {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 
-.button {
-    margin-top: 30px;
+.radio-view {
+    margin: 30px 0;
 }
 </style>
